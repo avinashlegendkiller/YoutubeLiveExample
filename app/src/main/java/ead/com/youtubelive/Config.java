7bb5d32821ad2d6524721e72d4a7f7ec5ce53e21
Avinash Gardas <avinash.gardas@adp.com>
2017-03-26 15:51:35 +0530

    initial commit

---
+++ app/src/main/java/ead/com/youtubelive/Config.java
@@ -0,0 +1,14 @@
+package ead.com.youtubelive;
+
+/**
+ * Created by gardasa on 3/19/17.
+ */
+
+public class Config {
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyDXuZ0WnrcItKpxEQByCtf3hrnz__FfcBs";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "MgEwodSawhA";
+}
